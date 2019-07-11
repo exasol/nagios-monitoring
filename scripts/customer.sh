009c82839523118d7631d4a764c000eda4c3dd43
Daniel Schlieder <daniel.schlieder@exasol.com>
2019-07-11 11:34:12 +0200

    fix for customer.sh

--- scripts/customer.sh
+++ scripts/customer.sh
@@ -3,15 +3,15 @@
 rm -rf /tmp/customer
 ENV=$1
 
-git clone https://github.com/danielschlieder/customer /tmp/customer >/dev/null 2>&1
-PENV="/tmp/customer/$ENV/"
+git clone https://github.com/danielschlieder/customer.git /tmp/customer >/dev/null 2>&1
+PENV="/tmp/customer/$ENV"
 if [[ -e "$PENV" ]]; then
 	if [[ -e "$PENV/checks" ]]; then
-		cp "$PENV/checks/*" /usr/lib/nagios/plugins
+		cp $PENV/checks/* /usr/lib/nagios/plugins
 	fi 
 	if [[ -e "$PENV/defs" ]]; then
 		mkdir -p /usr/share/icinga2/include/plugins-$ENV
-		cp "$PENV/defs/*" /usr/share/icinga2/include/plugins-$ENV
+		cp $PENV/defs/* /usr/share/icinga2/include/plugins-$ENV
 	fi
 	echo "include <plugins-$ENV>" >> /etc/icinga2/icinga2.conf
 fi 
