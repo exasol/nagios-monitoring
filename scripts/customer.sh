4aea66d8bc7ed5b53ed4d30cde181d41e836ad97
Daniel Schlieder <daniel.schlieder@exasol.com>
2019-07-11 13:06:28 +0200

    Fix for customer script

--- scripts/customer.sh
+++ scripts/customer.sh
@@ -2,16 +2,26 @@
 
 rm -rf /tmp/customer
 ENV=$1
-
+CHECKS=0
+DEFS=0
 git clone https://github.com/danielschlieder/customer.git /tmp/customer >/dev/null 2>&1
 PENV="/tmp/customer/$ENV"
 if [[ -e "$PENV" ]]; then
+	echo "Found checks suppllied by the customer for environemt $ENV" >&2
 	if [[ -e "$PENV/checks" ]]; then
-		cp $PENV/checks/* /usr/lib/nagios/plugins
+		echo "Copying checks" >&2
+		cp -arv $PENV/checks/* /usr/lib/nagios/plugins
+		CHECKS=1
 	fi 
 	if [[ -e "$PENV/defs" ]]; then
+		DEFS=1
 		mkdir -p /usr/share/icinga2/include/plugins-$ENV
 		cp $PENV/defs/* /usr/share/icinga2/include/plugins-$ENV
+		echo "include <plugins-$ENV>" >> /etc/icinga2/icinga2.conf
+	else
+		if [[ "$CHECKS" == "1" ]]; then
+			echo "Customer supplied checks without a definition! Please supply the definition in the /defs folder" >&2
+			exit 1
+		fi
 	fi
-	echo "include <plugins-$ENV>" >> /etc/icinga2/icinga2.conf
 fi 
